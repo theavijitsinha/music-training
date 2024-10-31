145816e69b7eb79a4fc0dac60c5c548ef72cc968
Avijit Sinha <theavijitsinha@gmail.com>
2024-10-31 09:06:17 +0000

    Use piano samples

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import {
-  useEffect,
-} from 'react';
+  useState,
+} from "react";
 import {
-  Synth,
+  Sampler,
+  start as audioStart,
+  now as toneNow,
 } from "tone";
 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    //create a synth and connect it to the main output (your speakers)
-    const synth = new Synth().toDestination();
+  const [toneLoaded, setToneLoaded] = useState(false);
+  const [audioStarted, setAudioStarted] = useState(false);
 
+  const sampler = new Sampler({
+    urls: {
+      "C4": "C4.mp3",
+      "D#4": "Ds4.mp3",
+      "F#4": "Fs4.mp3",
+      "A4": "A4.mp3",
+    },
+    release: 1,
+    baseUrl: "https://tonejs.github.io/audio/salamander/",
+    onload: () => {
+      setToneLoaded(true);
+    },
+  }).toDestination();
+
+  const initializeAudio = () => {
+    audioStart()
+      .then(() => {
+        setAudioStarted(true);
+      });
+  }
+
+  const playC4E4 = () => {
     //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
-  })
+    sampler.triggerAttackRelease("C4", "8n", toneNow());
+    //play a middle 'E' for the duration of an 8th note
+    sampler.triggerAttackRelease("E4", "8n", toneNow() + 0.5);
+  };
 
   return (
     <div className="App">
@@ -32,6 +57,8 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={initializeAudio}>Start Audio</button>
+        <button disabled={!(toneLoaded && audioStarted)} onClick={playC4E4}>Play!!</button>
       </header>
     </div>
   );
